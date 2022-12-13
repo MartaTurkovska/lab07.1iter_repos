71d4ffaa9209445fa966491aba70eb2ec46ea066
Marta <turkovska05@gmail.com>
2022-12-13 22:54:06 +0200

    fuuuck

---
+++ UnitTest7.1iter/UnitTest7.1iter.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest71iter
+{
+	TEST_CLASS(UnitTest71iter)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+			Assert::AreEqual ;
+		}
+
+	};
+}
